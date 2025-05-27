4b8c3e11684c9ce3acd2bb702ef7e8b81962fa34
syedamush <amushahmed2244@gmail.com>
2025-05-27 23:06:31 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DevOps-Project/', // 👈 this must match your GitHub repo name , Yes it matches , HOPE IT RUNS!!
 })
